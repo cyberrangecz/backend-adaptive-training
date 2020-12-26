c263cff91aaefa0943d0a428ca5b1b1770541dda
Jan Tymel <410388@mail.muni.cz>
2020-12-26 18:12:21 +0100

    Revert "Remove trainingDefinitionId parameter from LevelOperationsController"
    This reverts commit 446806888173a4b2fac7b5bce54b4232a77ba781.

--- src/main/java/com/example/demo/controller/LevelOperationsController.java
+++ src/main/java/com/example/demo/controller/LevelOperationsController.java
@@ -18,10 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/levels")
+@RequestMapping("/{definitionId}/levels")
 @CrossOrigin(origins = "*", allowCredentials = "true", allowedHeaders = "*",
              methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
-@Api(value = "/levels", tags = {"Level operations"})
+@Api(value = "/{definitionId}/levels", tags = {"Level operations"})
 public class LevelOperationsController {
 
     private final LevelOperationsService levelOperationsService;
@@ -36,10 +36,12 @@ public class LevelOperationsController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Level orders changed"),
                            @ApiResponse(code = 500, message = "Unexpected application error")})
     public void createInfoLevel(
+        @ApiParam(value = "Training definition ID", required = true) @PathVariable(name = "definitionId")
+            Long definitionId,
         @ApiParam(value = "Level ID - from", required = true) @PathVariable(name = "levelIdFrom") Long levelIdFrom,
         @ApiParam(value = "Level ID - to", required = true) @PathVariable(name = "levelIdTo") Long levelIdTo) {
 
-        levelOperationsService.swapLevelsOrder(levelIdFrom, levelIdTo);
+        levelOperationsService.swapLevelsOrder(definitionId, levelIdFrom, levelIdTo);
     }
 
     @DeleteMapping(value = "/{levelId}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -47,7 +49,9 @@ public class LevelOperationsController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Level deleted"),
                            @ApiResponse(code = 500, message = "Unexpected application error")})
     public void deleteLevel(
+        @ApiParam(value = "Training definition ID", required = true) @PathVariable(name = "definitionId")
+            Long definitionId,
         @ApiParam(value = "Level ID - from", required = true) @PathVariable(name = "levelId") Long levelId) {
-        levelOperationsService.deleteLevel(levelId);
+        levelOperationsService.deleteLevel(definitionId, levelId);
     }
 }

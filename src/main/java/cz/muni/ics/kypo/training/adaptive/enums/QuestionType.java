4a1bb3a37afd6d8dc427c67590388ec699c57ae2
Dominik Pilár <xpilar3@fi.muni.cz>
2021-02-25 09:23:04 +0100

    Added descriptions of the questionnaire types.

--- src/main/java/cz/muni/ics/kypo/training/adaptive/enums/QuestionType.java
+++ src/main/java/cz/muni/ics/kypo/training/adaptive/enums/QuestionType.java
@@ -1,7 +1,19 @@
 package cz.muni.ics.kypo.training.adaptive.enums;
 
+/**
+ * The enum Question type.
+ */
 public enum QuestionType {
+    /**
+     * Free-form question type.
+     */
     FFQ,
+    /**
+     * Multiple choice question type.
+     */
     MCQ,
+    /**
+     * Rate form question type.
+     */
     RFQ;
 }

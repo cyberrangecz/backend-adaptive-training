c36227dc605c0bd6d5f24bb56536cb51201c045f
Lukáš <445519@mail.muni.cz>
2024-04-02 15:45:45 +0200

    Move question_required to different DTO

--- src/main/java/cz/muni/ics/kypo/training/adaptive/dto/questionnaire/QuestionDTO.java
+++ src/main/java/cz/muni/ics/kypo/training/adaptive/dto/questionnaire/QuestionDTO.java
@@ -14,9 +14,6 @@ public class QuestionDTO extends AbstractQuestionDTO {
 
     @ApiModelProperty(value = "Question ID. Leave blank if a new question is added", required = true, example = "1")
     private Long id;
-    @ApiModelProperty(value = "Sign if the question must be answered by the participant or not.", example = "true")
-    @NotNull(message = "{question.answerRequired.NotNull.message}")
-    private boolean answerRequired;
 
     public Long getId() {
         return id;
@@ -26,33 +23,24 @@ public class QuestionDTO extends AbstractQuestionDTO {
         this.id = id;
     }
 
-    public boolean isAnswerRequired() {
-        return answerRequired;
-    }
-
-    public void setAnswerRequired(boolean answerRequired) {
-        this.answerRequired = answerRequired;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         QuestionDTO that = (QuestionDTO) o;
-        return Objects.equals(id, that.id) && Objects.equals(answerRequired, that.answerRequired);
+        return Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), id, answerRequired);
+        return Objects.hash(super.hashCode(), id);
     }
 
     @Override
     public String toString() {
         return "QuestionDTO{" +
                 "id=" + id +
-                "answerRequired=" + answerRequired +
                 "} " + super.toString();
     }
 }

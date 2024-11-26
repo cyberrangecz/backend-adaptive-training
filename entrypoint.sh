76f3951f3dc072d2bf8a447317cdc1eb0a074fe7
Andrej Kiruscanka <kiruscanka@cshub.cz>
2024-11-26 13:44:41 +0100

    update pom update MAVEN_CLI_OPTS refactor docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,4 +37,4 @@ do
     wait_for_it ${i}
 done
 
-exec java -Dspring.config.location=/app/etc/kypo-adaptive-training.properties -jar /app/kypo-adaptive-training.jar
+exec java -Dspring.config.location=/app/etc/$PROJECT_ARTIFACT_ID.properties -jar /app/$PROJECT_ARTIFACT_ID.jar
